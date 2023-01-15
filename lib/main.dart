0251ef63765983a05e6028d16e7a20e1bd53528d
nick747 <84738664+nick747@users.noreply.github.com>
2023-01-15 14:26:25 +0100

    Modified files position/path

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
-import 'package:ecommerce_flutter_app/src/app.dart';
+import 'screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   const app = ECommerceApp();
   runApp(app);
+}
+class ECommerceApp extends StatelessWidget {
+  const ECommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
 }
--- lib/screens/product_card.dart
+++ lib/screens/product_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../models/product.dart';
+import '../models/product.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
