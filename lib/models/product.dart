eef255c5fa08475e2ec20447bd5752703c640139
luigimicco <luigi.micco@gmail.com>
2023-03-04 18:25:27 +0100

    new structure for data

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,5 +1,20 @@
+var data = [
+  {
+    'name': "iPhone 14 Pro Max",
+    'description': [
+      "L'iPhone 14 Pro Max è uno dei modelli di fascia alta della linea iPhone 2022. Ha un display XDR da 6,7 pollici, fotocamera principale tripla, processore A16 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
+      'The iPhone 14 Pro Max is one of the high-end models in the 2022 iPhone lineup. It has a 6.7-inch XDR display, triple primary camera, A16 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is waterproof.'
+    ],
+    'price': [1099, 1489, 1199],
+    'imageUrl': 'assets/images/i14pm.png',
+    'imageUrlDt': 'assets/images/i14pm_dt.png',
+    'starred': false,
+    'bought': false
+  },
+];
+
 var description = {
-  0:[
+  0: [
     "L'iPhone 14 Pro Max è uno dei modelli di fascia alta della linea iPhone 2022. Ha un display XDR da 6,7 pollici, fotocamera principale tripla, processore A16 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
     "L'iPhone 14 Pro è uno dei modelli di fascia alta della linea iPhone 2022. Ha un display XDR da 6,1 pollici, fotocamera principale tripla, processore A16 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
     "L'iPhone 14 Plus il modello plus della linea di iPhone 2022.Ha un display XDR da 6,7 pollici, fotocamera principale doppia, processore A15 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
@@ -9,7 +24,7 @@ var description = {
     "L'iPhone 12 è il modello base della linea di iPhone 2020. Ha un display OLED da 6,1 pollici, fotocamera principale doppia, processore A14 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
     "L'iPhone SE (seconda generazione) è uno smartphone più economico di Apple con caratteristiche simili a quelle dei modelli di fascia alta. Include un display da 4,7 pollici, fotocamera principale da 12MP, processore A15 Bionic, Touch ID, ricarica wireless e il sistema operativo iOS 16. È stato presentato il 15 aprile 2020 e messo in vendita a partire dallo stesso giorno.",
   ],
-  1:[
+  1: [
     "The iPhone 14 Pro Max is one of the high-end models in the 2022 iPhone lineup. It has a 6.7-inch XDR display, triple primary camera, A16 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is waterproof.",
     "The iPhone 14 Pro is one of the high-end models in the 2022 iPhone lineup. It has a 6.1-inch XDR display, triple primary camera, A16 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is waterproof.",
     "The iPhone 14 Plus is the plus model in the 2022 iPhone lineup. It has a 6.7-inch XDR display, dual primary cameras, A15 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is water resistant .",
@@ -56,8 +71,8 @@ var price = {
 
 class Product {
   final String name;
-  final int description;
-  final int price;
+  final List<String> description;
+  final List<double> price;
   final String imageUrl;
   final String imageUrlDt;
   bool starred;
@@ -72,4 +87,39 @@ class Product {
     this.starred,
     this.bought,
   );
+
+  String getDescription(int language) {
+    return description[language];
+  }
+
+  double getPrice(int currency) {
+    return price[currency];
+  }
+}
+
+List<Product> productList = [];
+
+List<Product> populateList() {
+  List<Product> list = [];
+  for (Map<String, dynamic> item in data) {
+    var newItem = Product(
+        item['name'],
+        item['description'],
+        item['price'],
+        item['imageUrl'],
+        item['imageUrlDt'],
+        item['starred'] as bool,
+        item['bought'] as bool);
+    list.add(newItem);
+  }
+  return list;
+}
+
+double sumPricesList(List<Product> productList, int currency) {
+  double total = 0;
+  for (Product product in productList) {
+    total += product.price[currency];
+  }
+
+  return total;
 }
