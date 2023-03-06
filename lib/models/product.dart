c26e292363bccd97047defd75f32705a1bb7758b
nick747 <84738664+nick747@users.noreply.github.com>
2023-03-06 07:18:07 +0100

    Added French Descriptions

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,7 +3,8 @@ var data = [
     'name': "iPhone 14 Pro Max",
     'description': [
       "L'iPhone 14 Pro Max è uno dei modelli di fascia alta della linea iPhone 2022. Ha un display XDR da 6,7 pollici, fotocamera principale tripla, processore A16 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
-      'The iPhone 14 Pro Max is one of the high-end models in the 2022 iPhone lineup. It has a 6.7-inch XDR display, triple primary camera, A16 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is waterproof.'
+      'The iPhone 14 Pro Max is one of the high-end models in the 2022 iPhone lineup. It has a 6.7-inch XDR display, triple primary camera, A16 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is waterproof.',
+      "L'iPhone 14 Pro Max est l'un des modèles haut de gamme de la gamme iPhone 2022. Il dispose d'un écran XDR de 6,7 pouces, d'un triple appareil photo principal, d'un processeur A16 Bionic, de la 5G, de Face ID, d'une charge sans fil, du dernier système d'exploitation iOS et est étanche.",
     ],
     'price': [
       1099.0,
@@ -20,6 +21,7 @@ var data = [
     'description': [
       "L'iPhone 14 Pro è uno dei modelli di fascia alta della linea iPhone 2022. Ha un display XDR da 6,1 pollici, fotocamera principale tripla, processore A16 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
       "The iPhone 14 Pro is one of the high-end models in the 2022 iPhone lineup. It has a 6.1-inch XDR display, triple primary camera, A16 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is waterproof.",
+      "L'iPhone 14 Pro est l'un des modèles haut de gamme de la gamme d'iPhone 2022. Il dispose d'un écran XDR de 6,1 pouces, d'un triple appareil photo principal, d'un processeur A16 Bionic, de la 5G, de Face ID, de la recharge sans fil, du dernier système d'exploitation iOS, et est étanche.",
     ],
     'price': [
       999.0,
@@ -36,6 +38,7 @@ var data = [
     'description': [
       "L'iPhone 14 Plus il modello plus della linea di iPhone 2022.Ha un display XDR da 6,7 pollici, fotocamera principale doppia, processore A15 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
       "The iPhone 14 Plus is the plus model in the 2022 iPhone lineup. It has a 6.7-inch XDR display, dual primary cameras, A15 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is water resistant .",
+      "L'iPhone 14 Plus est le modèle plus de la gamme d'iPhone 2022. Il dispose d'un écran XDR de 6,7 pouces, de deux caméras principales, d'un processeur A15 Bionic, de la 5G, de Face ID, de la recharge sans fil, du dernier système d'exploitation iOS et est résistant à l'eau .",
     ],
     'price': [
       899.0,
@@ -52,6 +55,7 @@ var data = [
     'description': [
       "L'iPhone 14 il modello base della linea di iPhone 2022.Ha un display XDR da 6,1 pollici, fotocamera principale doppia, processore A15 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
       "The iPhone 14 is the base model of the 2022 iPhone lineup. It has a 6.1-inch XDR display, dual primary cameras, A15 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is water resistant.",
+      "L'iPhone 14 est le modèle de base de la gamme d'iPhone 2022. Il dispose d'un écran XDR de 6,1 pouces, de deux caméras principales, d'un processeur A15 Bionic, de la 5G, de Face ID, de la recharge sans fil, du dernier système d'exploitation iOS et est résistant à l'eau. ",
     ],
     'price': [
       799.0,
@@ -68,6 +72,7 @@ var data = [
     'description': [
       "L'iPhone 13 Mini è il modello mini della linea di iPhone 2021. Ha un display XDR da 5,4 pollici, fotocamera principale doppia, processore A15 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
       "The iPhone 13 Mini is the mini model in the 2021 iPhone lineup. It has a 5.4-inch XDR display, dual primary cameras, A15 Bionic processor, 5G, Face ID, wireless charging, the latest iOS operating system, and is water resistant. waterfall.",
+      "L'iPhone 13 Mini est le mini modèle de la gamme d'iPhone 2021. Il dispose d'un écran XDR de 5,4 pouces, de deux caméras principales, d'un processeur A15 Bionic, de la 5G, de Face ID, de la recharge sans fil, du dernier système d'exploitation iOS et est résistant à l'eau . cascade.",
     ],
     'price': [
       599.0,
@@ -84,6 +89,7 @@ var data = [
     'description': [
       "L'iPhone 13 è il modello base della linea di iPhone 2021. Ha un display XDR da 6,1 pollici, fotocamera principale doppia, processore A15 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
       "The iPhone 13 is the base model in the 2021 iPhone lineup. It has a 6.1-inch XDR display, dual primary camera, A15 Bionic processor, 5G, Face ID, wireless charging, latest iOS operating system, and is water resistant .",
+      "L'iPhone 13 est le modèle de base de la gamme d'iPhone 2021. Il dispose d'un écran XDR de 6,1 pouces, d'un double appareil photo principal, d'un processeur A15 Bionic, de la 5G, de Face ID, d'un chargement sans fil, du dernier système d'exploitation iOS et est résistant à l'eau.",
     ],
     'price': [
       699.0,
@@ -100,6 +106,7 @@ var data = [
     'description': [
       "L'iPhone 12 è il modello base della linea di iPhone 2020. Ha un display OLED da 6,1 pollici, fotocamera principale doppia, processore A14 Bionic, 5G, Face ID, ricarica wireless, ultimo sistema operativo iOS ed è resistente all'acqua.",
       "The iPhone 12 is the base model in the 2020 iPhone lineup. It has a 6.1-inch OLED display, dual primary camera, A14 Bionic processor, 5G, Face ID, wireless charging, latest iOS operating system, and is water resistant .",
+      "L'iPhone 12 est le modèle de base de la gamme d'iPhone 2020. Il dispose d'un écran OLED de 6,1 pouces, d'un double appareil photo principal, d'un processeur A14 Bionic, de la 5G, de l'identification faciale, de la recharge sans fil, du dernier système d'exploitation iOS et est résistant à l'eau.",
     ],
     'price': [
       599.0,
@@ -116,6 +123,7 @@ var data = [
     'description': [
       "L'iPhone SE (seconda generazione) è uno smartphone più economico di Apple con caratteristiche simili a quelle dei modelli di fascia alta. Include un display da 4,7 pollici, fotocamera principale da 12MP, processore A15 Bionic, Touch ID, ricarica wireless e il sistema operativo iOS 16. È stato presentato il 15 aprile 2020 e messo in vendita a partire dallo stesso giorno.",
       "The iPhone SE (2nd generation) is a more affordable smartphone from Apple with features similar to the high-end models. It includes a 4.7-inch display, 12MP main camera, A15 Bionic processor, Touch ID, wireless charging and the iOS 16 operating system. It was unveiled on April 15, 2020 and went on sale from the same day.",
+      "L'iPhone SE (2e génération) est un smartphone plus abordable d'Apple avec des fonctionnalités similaires aux modèles haut de gamme. Il comprend un écran de 4,7 pouces, un appareil photo principal de 12 MP, un processeur A15 Bionic, Touch ID, le chargement sans fil et l'iOS 16 système d'exploitation. Il a été dévoilé le 15 avril 2020 et mis en vente à partir du même jour.",
     ],
     'price': [
       429.0,
